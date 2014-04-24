a71604b8f757d455ed353bed6765743d4f3f2e40
Ashish <srits.ashish@gmail.com>
2014-04-24 18:49:15 +0530

    commit all changes

---
+++ LazyLoadingTests/LazyLoadingTests.m
@@ -0,0 +1,34 @@
+//
+//  LazyLoadingTests.m
+//  LazyLoadingTests
+//
+//  Created by Ashish Tripathi on 21/04/14.
+//  Copyright (c) 2014 Ashish Tripathi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LazyLoadingTests : XCTestCase
+
+@end
+
+@implementation LazyLoadingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
